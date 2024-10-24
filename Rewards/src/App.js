6dff456e80a19e7e3bb12fdcbae2c62c499ac70c
SaiPraneeth998 <praneeth.reactjs19@gmail.com>
2024-10-24 12:10:44 +0530

    intial commits

---
+++ Rewards/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import MainRouteComponent from "./Components/MainRouteComponent";
+
+const App = () => {
+  const [customers, setCustomers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("userData.json");
+        const data = await response.json();
+        setCustomers(data);
+      } catch (e) {
+        console.log(e, "error");
+      } finally {
+        setLoading(false); // Stop the loading state once data is fetched
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <MainRouteComponent customers={customers} />
+      )}
+    </>
+  );
+};
+
+export default App;
---
+++ Rewards/src/Components/CustomerPoints.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CustomerPoints = ({ month, points }) => {
+  return (
+    <p>
+      {month}: {points} points
+    </p>
+  );
+};
+export default CustomerPoints;
